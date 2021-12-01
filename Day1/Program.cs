d8ea1e92aca7c208e4f1c23cf92f27454fe6f3f6
Rudolf Németh <rnemeth17@gmail.com>
2021-12-01 09:47:17 +0100

    AoC day1

--- Day1/Program.cs
+++ Day1/Program.cs
@@ -1,2 +1,42 @@
 ﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+using Day1;
+
+var increasedCount = 0;
+var increasedWindowsCount = 0;
+
+// PART ONE & PART TWO
+// foreach (var line in System.IO.File.ReadLines(""))
+using (var fileStream = new FileStream("input.txt", FileMode.Open, FileAccess.Read))
+using (var streamReader = new StreamReader(fileStream))
+{
+    string line = string.Empty;
+    int? previouseValue = null;
+
+    Window window1 = new Window();
+    Window window2 = new Window();
+
+    while ((line = streamReader.ReadLine())
+        != null)
+    {
+        if (int.TryParse(line, out int value))
+        {
+            if (previouseValue.HasValue)
+                window2.AddValue(value);
+
+            if (window1.IsValid() && window2.IsValid())
+                if (window1.GetSum() < window2.GetSum())
+                    increasedWindowsCount++;
+
+            window1.AddValue(value);
+
+            if (previouseValue.HasValue && previouseValue.Value < value)
+            {
+                increasedCount++;
+            }
+            previouseValue = value;
+        }
+    }
+}
+
+Console.WriteLine("Value incresed: {0} times.", increasedCount);
+Console.WriteLine("Window value incresed: {0} times.", increasedWindowsCount);
---
+++ Day1/Window.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day1
+{
+    internal class Window
+    {
+        public Window()
+        {
+            Values = new int?[3];
+        }
+
+        public int?[] Values { get; private set; }
+
+        public void AddValue(int value)
+        {
+            Values[2] = Values[1];
+            Values[1] = Values[0];
+            Values[0] = value;
+        }
+
+        public int GetSum()
+            => (Values[0] ?? 0) + (Values[1] ?? 0) + (Values[2] ?? 0);
+
+        public bool IsValid()
+            => Values[0].HasValue && Values[1].HasValue && Values[2].HasValue;
+    }
+}
