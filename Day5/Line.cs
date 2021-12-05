5e207219bf463f5820612ccd832bcfd2103962f8
Rudolf Németh <rnemeth17@gmail.com>
2021-12-05 12:31:44 +0100

    AoC day5

---
+++ Day5/Line.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day5
+{
+    internal class Line
+    {
+        public Line(int x1, int y1, int x2, int y2)
+        {
+            FromPoint = new Point(x1, y1);
+            ToPoint = new Point(x2, y2);
+        }
+
+        public Point FromPoint { get; private set; }
+        public Point ToPoint { get; private set; }
+
+        public bool IsStraightLine()
+            => FromPoint.X == ToPoint.X || FromPoint.Y == ToPoint.Y;
+
+        public override string ToString()
+            => $"{FromPoint.X},{FromPoint.Y} -> {ToPoint.X},{ToPoint.Y}";
+
+        public IEnumerable<Point> GetAllPoints()
+        {
+            var points = new List<Point>();
+            points.Add(FromPoint);
+            points.Add(ToPoint);
+
+            if (FromPoint.X == ToPoint.X)
+            {
+                var y = ToPoint.Y > FromPoint.Y ? 1 : -1;
+                while(ToPoint.Y != FromPoint.Y + y)
+                {
+                    points.Add(new Point(FromPoint.X, FromPoint.Y + y));
+
+                    if (ToPoint.Y > FromPoint.Y)
+                        y++;
+                    else
+                        y--;
+                }
+            }
+            else if (FromPoint.Y == ToPoint.Y)
+            {
+                var x = ToPoint.X > FromPoint.X ? 1 : -1;
+                while (ToPoint.X != FromPoint.X + x)
+                {
+                    points.Add(new Point(FromPoint.X + x, FromPoint.Y));
+
+                    if (ToPoint.X > FromPoint.X)
+                        x++;
+                    else
+                        x--;
+                }
+            }
+            else
+            {
+                // is 45 diagonal
+                var x = ToPoint.X > FromPoint.X ? 1 : -1;
+                var y = ToPoint.Y > FromPoint.Y ? 1 : -1;
+
+                while (ToPoint.X != FromPoint.X + x && ToPoint.Y != FromPoint.Y + y)
+                {
+                    points.Add(new Point(FromPoint.X + x, FromPoint.Y + y));
+
+                    if (ToPoint.X > FromPoint.X)
+                        x++;
+                    else
+                        x--;
+
+                    if (ToPoint.Y > FromPoint.Y)
+                        y++;
+                    else
+                        y--;
+                }
+            }
+
+            return points;
+        }
+    }
+}
---
+++ Day5/Program.cs
@@ -0,0 +1,92 @@
+﻿using Day5;
+using System.Diagnostics;
+using System.Drawing;
+using System.Text;
+
+var data = File.ReadLines("input.txt");
+
+var parsedLines = new List<Line>();
+
+foreach (var line in data)
+{
+    var points = line.Split(new string[] { "->", "," }, StringSplitOptions.TrimEntries).Select(p => int.Parse(p)).ToArray();
+
+    Debug.Assert(points.Length == 4);
+
+    parsedLines.Add(new Line(points[0], points[1], points[2], points[3]));
+}
+
+Console.WriteLine("# of dangerous points: {0}", CalculateDangerousPoints(parsedLines));
+
+static int CalculateDangerousPoints(IEnumerable<Line> lines)
+{
+    var coveredPoints = new Dictionary<Point, int>();
+
+    var maxX = 0;
+    var maxY = 0;
+
+    foreach (var line in lines)
+    {
+        // Part1
+        //if (line.IsStraightLine())
+        //{
+            foreach (var p in line.GetAllPoints())
+            {
+                if (p.X > maxX) maxX = p.X;
+                if (p.Y > maxY) maxY = p.Y;
+
+                if (coveredPoints.ContainsKey(p))
+                    coveredPoints[p] = coveredPoints[p] + 1;
+                else
+                    coveredPoints.Add(p, 1);
+            }
+        //}
+    }
+
+    DrawToFile("out.txt", coveredPoints, maxX, maxY);
+
+    return coveredPoints.Count(p => p.Value > 1);
+}
+
+static void DrawToConsole(Dictionary<Point, int> points, int maxX, int maxY)
+{
+    for (var y = 0; y <= maxY; y++)
+    {
+        for (var x = 0; x <= maxX; x++)
+        {
+            var point = new Point(x, y);
+            if (points.ContainsKey(point))
+            {
+                Console.Write(points[point]);
+            }
+            else
+            {
+                Console.Write('.');
+            }
+        }
+        Console.WriteLine();
+    }
+}
+
+static void DrawToFile(string filePath, Dictionary<Point, int> points, int maxX, int maxY)
+{
+    var gridBuilder = new StringBuilder();
+    for (var y = 0; y <= maxY; y++)
+    {
+        for (var x = 0; x <= maxX; x++)
+        {
+            var point = new Point(x, y);
+            if (points.ContainsKey(point))
+            {
+                gridBuilder.Append(points[point]);
+            }
+            else
+            {
+                gridBuilder.Append('.');
+            }
+        }
+        gridBuilder.Append(Environment.NewLine);
+    }
+
+    System.IO.File.WriteAllText(filePath, gridBuilder.ToString());
+}
