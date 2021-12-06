e5ce5a990e8b55bb9ab88b182cca00dba66a7d7f
Rudolf Németh <rnemeth17@gmail.com>
2021-12-06 17:06:23 +0100

    AoC day6

---
+++ Day6/Lanternfish.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Day6
+{
+    internal class Lanternfish
+    {
+        private int _internalBase = 6;
+
+        public Lanternfish(int timer)
+            : this(timer, 6)
+        {
+
+        }
+
+        public Lanternfish(int timer, int baseTimer)
+        {
+            InternalTimer = timer;
+            _internalBase = baseTimer;
+        }
+
+        public int InternalTimer { get; private set; }
+
+        public Lanternfish Tick()
+        {
+            if (InternalTimer == 0)
+            {
+                InternalTimer = _internalBase;
+                return new Lanternfish(_internalBase + 2);
+            }
+            else
+            {
+                InternalTimer--;
+                return null;
+            }
+        }
+    }
+}
---
+++ Day6/Program.cs
@@ -0,0 +1,47 @@
+﻿using Day6;
+using System.Numerics;
+
+var data = File.ReadLines("input.txt");
+
+var populationBasedOnTimer = new Dictionary<int, List<Lanternfish>>();
+
+for (var variant = 0; variant <= 100; variant++)
+{
+    var l = new Lanternfish(variant);
+
+    var fishPopulation = new List<Lanternfish>();
+    var newPopulation = new List<Lanternfish>();
+
+    fishPopulation.Add(l);
+
+    for (var day = 1; day <= 128; day++)
+    {
+        foreach (var fish in fishPopulation)
+        {
+            var newfish = fish.Tick();
+
+            if (newfish != null)
+            {
+                newPopulation.Add(newfish);
+            }
+        }
+
+        fishPopulation.AddRange(newPopulation);
+        newPopulation.Clear();
+    }
+
+    Console.WriteLine("Variant #{0}, population: {1}", variant, fishPopulation.Count);
+    populationBasedOnTimer.Add(variant, fishPopulation);
+}
+
+var finalCount = BigInteger.Zero;
+
+foreach (var row in data)
+    foreach (var value in row.Split(',').Select(d => int.Parse(d)))
+    {
+        var populationAfter128Days = populationBasedOnTimer[value];
+        foreach (var fish in populationAfter128Days)
+            finalCount += populationBasedOnTimer[fish.InternalTimer].Count;
+    }
+
+Console.WriteLine("Lanternfish population after 256 days: {0}", finalCount);
