4fc370c6f01a05cf0fce7d2631ac89056cb74ea9
Rudolf Németh <rnemeth17@gmail.com>
2021-12-10 11:24:44 +0100

    AoC day10

---
+++ Day10/Program.cs
@@ -0,0 +1,94 @@
+﻿using System.Diagnostics;
+using System.Numerics;
+using System.Text;
+
+var syntaxErrorPoints = new Dictionary<char, int>()
+{
+    { ')', 3 },
+    { ']', 57 },
+    { '}', 1197 },
+    { '>', 25137 },
+};
+
+var lines = File.ReadLines("input.txt");
+
+var scores = new List<BigInteger>();
+
+var total = 0;
+foreach (var line in lines)
+{
+    var corrupted = false;
+    var brackets = new Stack<char>();
+
+    foreach (var c in line)
+    {
+        switch (c)
+        {
+            case '(':
+            case '[':
+            case '{':
+            case '<':
+                brackets.Push(c);
+                break;
+            case ')':
+            case ']':
+            case '}':
+            case '>':
+                if (!IsMatch(brackets.Pop(), c))
+                {
+                    total += syntaxErrorPoints[c];
+                    corrupted = true;
+                }
+
+                break;
+            default:
+                break;
+        }
+    }
+
+    if (!corrupted)
+    {
+        var completionSequence = new StringBuilder();
+        while (brackets.Count > 0)
+            completionSequence.Append(GetPair(brackets.Pop()));
+
+        var score = CalculateCompletionScore(completionSequence.ToString());
+
+        Debug.Assert(score > 0);
+
+        scores.Add(score);
+    }
+}
+
+scores.Sort();
+
+Console.WriteLine("total syntax error score: {0}", total);
+
+Console.WriteLine("Middle completion score: {0}", scores[scores.Count / 2 + 1]);
+
+static bool IsMatch(char opening, char closing)
+    =>  opening == '(' && closing == ')' ||
+        opening == '[' && closing == ']' ||
+        opening == '{' && closing == '}' ||
+        opening == '<' && closing == '>';
+
+static char GetPair(char opening)
+    => opening == '(' ? ')' : (opening == '[' ? ']' : (opening == '{' ? '}' : '>'));
+        
+
+static BigInteger CalculateCompletionScore(string completionSequence)
+{
+    var completionPoints = new Dictionary<char, int>()
+    {
+        { ')', 1 },
+        { ']', 2 },
+        { '}', 3 },
+        { '>', 4 },
+    };
+
+    var score = BigInteger.Zero;
+    foreach (var c in completionSequence)
+        score += (score * 5) + completionPoints[c];
+    
+    return score;
+}
