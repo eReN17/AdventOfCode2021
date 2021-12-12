6da9534d84274da96ed07fcfd46fee9145cc5677
Rudolf Németh <rnemeth17@gmail.com>
2021-12-12 14:44:55 +0100

    AoC day12

---
+++ Day12/Connection.cs
@@ -0,0 +1,57 @@
+﻿namespace Day12
+{
+    internal class Connection : ICloneable
+    {
+        private readonly List<string> _nodes = new List<string>();
+
+        private bool _smallCaveLock = false;
+
+        private string _lastNode = string.Empty;
+
+        public string GetLastNode()
+            => _lastNode;
+
+        public bool AddNode(string node)
+        { 
+            if (IsCaveSmall(node))
+            {
+                if (_nodes.Contains(node))
+                {
+                    if (_smallCaveLock)
+                        return false;
+
+                    _smallCaveLock = true;
+                }
+            }
+
+            _lastNode = node;
+            _nodes.Add(node);
+            return true;
+        }
+
+        private bool IsCaveSmall(string cave)
+        {
+            foreach (var c in cave)
+                if (c >= 'A' && c <= 'Z')
+                    return false;
+
+            return true;
+        }
+
+        public override string ToString()
+            => string.Join("-", _nodes);
+
+        public object Clone()
+        {
+            var connection = new Connection();
+
+            foreach (var n in _nodes)
+                connection.AddNode(n);
+
+            return connection;
+        }
+
+        public Connection CloneMe()
+            => (Connection)Clone();
+    }
+}
---
+++ Day12/Program.cs
@@ -0,0 +1,80 @@
+﻿using Day12;
+
+const string Start = "start";
+const string End = "end";
+
+var lines = File.ReadLines("input.txt");
+
+var connections = ConstructConnections(lines);
+
+var paths = new HashSet<string>();
+
+foreach (var start in from p in connections where p.Key == Start select p)
+{
+    var connection = new Connection();
+
+    connection.AddNode(start.Key);
+    connection.AddNode(start.Value);
+
+    ConstructPathsRecursive(connection, paths, connections);
+}
+
+foreach (var p in paths)
+{
+    Console.WriteLine(p);
+}
+
+Console.WriteLine("Constructed paths {0}:", paths.Count);
+
+static void ConstructPathsRecursive(Connection connection, HashSet<string> paths, List<KeyValuePair<string, string>> connections)
+{
+    var connectedNodes = connections.Where(p => p.Key == connection.GetLastNode()).ToArray();
+
+    foreach (var n in connectedNodes)
+    {
+        var clonedConnection = connection.CloneMe();
+
+        if (n.Value == End)
+        {
+            clonedConnection.AddNode(n.Value);
+            paths.Add(clonedConnection.ToString());
+        }
+        else if (clonedConnection.AddNode(n.Value))
+        {
+            
+            ConstructPathsRecursive(clonedConnection, paths, connections);
+        }
+    }
+}
+
+static List<KeyValuePair<string, string>> ConstructConnections(IEnumerable<string> lines)
+{
+    var connections = new List<KeyValuePair<string, string>>();
+
+    foreach (var line in lines)
+    {
+        var lineSplit = line.Split('-');
+
+        if (lineSplit[0] == Start || lineSplit[1] == Start)
+        {
+            connections.Add(new KeyValuePair<string, string>(
+                lineSplit[0] == Start ? lineSplit[0] : lineSplit[1],
+                lineSplit[0] == Start ? lineSplit[1] : lineSplit[0]
+            ));
+        }
+        else if (lineSplit[0] == End || lineSplit[1] == End)
+        {
+            connections.Add(new KeyValuePair<string, string>(
+                lineSplit[0] == End ? lineSplit[1] : lineSplit[0],
+                lineSplit[0] == End ? lineSplit[0] : lineSplit[1]
+            ));
+        }
+        else
+        {
+            connections.Add(new KeyValuePair<string, string>(lineSplit[0], lineSplit[1]));
+            connections.Add(new KeyValuePair<string, string>(lineSplit[1], lineSplit[0]));
+        }
+    }
+
+    return connections;
+}
