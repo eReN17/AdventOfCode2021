d7a2863a572f3f36096001ac6ea576213e5683fa
Rudolf Németh <rnemeth17@gmail.com>
2021-12-13 16:58:33 +0100

    AoC day13

---
+++ Day13/Grid.cs
@@ -0,0 +1,80 @@
+﻿namespace Day13
+{
+    using System.Collections.Generic;
+    using System.Drawing;
+    using System.Text;
+
+    internal class Grid
+    {
+        private int _maxX, _maxY;
+
+        private readonly HashSet<Point> _points
+            = new HashSet<Point>();
+
+        public void AddPoint(int x, int y)
+        {
+            if (_maxX < x)
+                _maxX = x;
+
+            if (_maxY < y)
+                _maxY = y;
+
+            _points.Add(new Point(x, y));
+        }
+
+        public Grid FoldX(int x)
+        {
+            var foldedGrid = new Grid();
+
+            foreach (var p in _points)
+            {
+                if (p.X > x)
+                {
+                    foldedGrid.AddPoint(x - (p.X - x), p.Y);
+                }
+                else if (p.X < x)
+                {
+                    foldedGrid.AddPoint(p.X, p.Y);
+                }
+            }
+
+            return foldedGrid;
+        }
+
+        public Grid FoldY(int y)
+        {
+            var foldedGrid = new Grid();
+
+            foreach (var p in _points)
+            {
+                if (p.Y > y)
+                {
+                    foldedGrid.AddPoint(p.X, y - (p.Y - y));
+                }
+                else if (p.Y < y)
+                {
+                    foldedGrid.AddPoint(p.X, p.Y);
+                }
+            }
+
+            return foldedGrid;
+        }
+
+        public int GetPointsCount()
+            => _points.Count;
+
+        public override string ToString()
+        {
+            var grid = new StringBuilder();
+            for (var y = 0; y <= _maxY; y++)
+            {
+                for (var x = 0; x <= _maxX; x++)
+                    grid.Append( _points.Contains(new Point(x, y)) ? "#" : "." );
+
+                grid.Append(Environment.NewLine);
+            }
+
+            return grid.ToString();
+        }
+    }
+}
---
+++ Day13/Program.cs
@@ -0,0 +1,50 @@
+﻿using Day13;
+
+const string FoldString = "fold along ";
+
+var lines = File.ReadLines("input.txt");
+
+var grid = new Grid();
+
+var folds = new List<Tuple<char, int>>();
+
+foreach (var l in lines)
+{
+    var coords = l.Split(',');
+    if (coords.Length == 2)
+    {
+        grid.AddPoint(
+            int.Parse(coords[0]),
+            int.Parse(coords[1])
+        );
+    }
+    else
+    {
+        if (l.Contains(FoldString))
+        {
+            var foldData = l.Substring(FoldString.Length).Split('=');
+            folds.Add(new Tuple<char, int>(foldData[0].First(), int.Parse(foldData[1])));
+        }
+    }
+}
+
+var foldedGrid = Fold(grid, folds.First());
+
+//Console.WriteLine("Point count after 1 fold: {0}", foldedGrid.GetPointsCount());
+
+foreach (var f in folds)
+{
+    grid = Fold(grid, f);
+}
+
+Console.WriteLine(grid);
+
+static Grid Fold(Grid grid, Tuple<char, int> fold)
+{
+    if (fold.Item1 == 'x')
+        return grid.FoldX(fold.Item2);
+    else if (fold.Item1 == 'y')
+        return grid.FoldY(fold.Item2);
+
+    return grid;
+}
